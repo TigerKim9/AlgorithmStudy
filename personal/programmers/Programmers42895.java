5c6981b0b11e0865933d98c0788cc0e67c7fa7aa
TigerKim9 <90361096+TigerKim9@users.noreply.github.com>
2023-03-13 23:19:30 +0900

    Add files via upload

---
+++ personal/programmers/Programmers42895.java
@@ -0,0 +1,31 @@
+package programmers;
+
+import java.util.*;
+
+public class Programmers42895 {
+
+	public static int[] solution(String[] genres, int[] plays) {
+        int[] answer = {};
+        HashMap<Integer, String> genre = new HashMap<>();
+        HashMap<Integer, Integer> play = new HashMap<>();
+        StringBuilder sb = new StringBuilder();
+        
+        sb.reverse();
+        String d = "";
+        d.contains("3");
+        for (int i = 0; i < genres.length; i++) {
+			genre.put(i,genres[i]);
+			play.put(i, plays[i]);
+		}
+        
+        
+        System.out.println(genre);
+        System.out.println(play);
+        return answer;
+    }
+	public static void main(String[] args) {
+		String[] genres = new String[] { "classic", "pop", "classic", "classic", "pop" };
+		int [] plays = new int [] {500, 600, 150, 800, 2500};
+		System.out.println(Arrays.toString(solution(genres, plays)));
+	}
+}
