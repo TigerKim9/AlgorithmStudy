916a93328a8f843f0e694ec1f6563b2804fd532f
TigerKim9 <90361096+TigerKim9@users.noreply.github.com>
2023-03-27 23:49:55 +0900

    Add files via upload

---
+++ personal/programmers/ProgrammersTESTPro.java
@@ -0,0 +1,18 @@
+package programmers;
+
+import java.util.*;
+
+public class ProgrammersTESTPro {
+
+	public static int[] solution(int[] arr) {
+        int[] answer = {0,0};
+        answer[0] = arr[0]^arr[1]^arr[2];
+        Arrays.sort(arr);
+        return answer;
+    }
+	public static void main(String[] args) {
+//		String[] genres = new String[] { "classic", "pop", "classic", "classic", "pop" };
+		int [] plays = new int [] {10, 2, 3, 800, 2500};
+		System.out.println(Arrays.toString(solution(plays)));
+	}
+}
